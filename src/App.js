8b89b9734432c442b500c66cf6c8ab9bb72cc77f
AmrAhmedA <47694180+AmrAhmedA@users.noreply.github.com>
2020-09-11 11:42:59 +0200

    Add custom breakpoints

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import './App.css';
 // import RegisterForm from "./component/registerForm"
 import HorizontalFormStepper from "./component/horizontalFormStepper"
 import VerticalFormStepper from "./component/verticalFormStepper"
-import { useTheme } from '@material-ui/core/styles';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useMediaQuery, withWidth, createMuiTheme } from '@material-ui/core/';
+import PropTypes from 'prop-types';
 import React from 'react';
+
 
 const warningTitleCSS =
   "color:red; font-size:50px; font-weight: bold; -webkit-text-stroke: 1px black;";
@@ -12,17 +13,34 @@ setTimeout(
   console.log.bind(console, "%c#Amr Ahmed", warningTitleCSS),
   0
 )
-function App() {
-  const theme = useTheme();
+
+const theme = createMuiTheme({ //custom breakpoints
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 800,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
+})
+
+function App(props) {
   const matches = useMediaQuery(theme.breakpoints.down('xs'));
+  // const { width } = props;
+  // console.log(width);
   return (
     <React.StrictMode>
       {console.log(matches)}
       {matches ? <VerticalFormStepper /> :
         <HorizontalFormStepper />}
     </React.StrictMode>
-
   );
-}
 
-export default App;
+}
+App.propTypes = {
+  width: PropTypes.oneOf(['lg', 'md', 'sm', 'xl', 'xs']).isRequired,
+};
+
+export default withWidth()(App);
