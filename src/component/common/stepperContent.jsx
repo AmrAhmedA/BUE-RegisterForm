49e7ef875664e782070a40c1dba6d4b9ab2e65e3
AmrAhmedA <47694180+AmrAhmedA@users.noreply.github.com>
2020-09-14 19:31:56 +0200

    Modify register form name

--- src/component/common/stepperContent.jsx
+++ src/component/common/stepperContent.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { useState, useEffect } from "react";
-import RegisterForm from "../registerForm";
+import RegisterForm from "../accountSetupForm";
 
 export const useVerticalStyles = makeStyles((theme) => ({
   root: {
