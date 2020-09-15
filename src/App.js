bf53fe8aa9957273034f28cff5a7cc2c5ae1fa55
AmrAhmedA <47694180+AmrAhmedA@users.noreply.github.com>
2020-09-15 19:40:14 +0200

    Add dropDownMenu component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import PropTypes from 'prop-types';
 import { useMediaQuery, withWidth, createMuiTheme } from '@material-ui/core/';
 import { StepperContent } from "./component/common/stepperContent"
+import DropDownInputMenu from './component/common/dropDownInputMenu';
 
 
 const warningTitleCSS =
@@ -29,6 +30,7 @@ const theme = createMuiTheme({ //custom breakpoints
 function App(props) {
   const matches = useMediaQuery(theme.breakpoints.down('xs'));
   const stepContent = StepperContent();
+  const items = ["Male", "Female", "other"]
   // const { width } = props;
   // console.log(width);
   return (
@@ -36,6 +38,7 @@ function App(props) {
       {/* {console.log(matches)} */}
       {matches ? <VerticalFormStepper {...stepContent} /> :
         <HorizontalFormStepper {...stepContent} />}
+      <DropDownInputMenu items={items} />
     </React.StrictMode>
   );
 
---
+++ src/component/common/dropDownInputMenu.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import {
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+  makeStyles,
+} from "@material-ui/core/";
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
+const DropDownInputMenu = ({ items }) => {
+  const classes = useStyles();
+  const [age, setAge] = React.useState("");
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  return (
+    <FormControl className={classes.formControl}>
+      <InputLabel id="demo-simple-select-label">Age</InputLabel>
+      <Select
+        labelId="demo-simple-select-label"
+        id="drop-down-input"
+        value={age}
+        onChange={handleChange}
+      >
+        {items.map((item) => (
+          <MenuItem key={item} value={item}>
+            {item}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+};
+
+export default DropDownInputMenu;
