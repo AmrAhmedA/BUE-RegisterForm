ad90e09ba6944ed627be1f8234eddfde3fdbf669
AmrAhmedA <47694180+AmrAhmedA@users.noreply.github.com>
2020-09-17 16:07:10 +0200

    Add fade transition

--- src/component/forms/accountSetupForm.jsx
+++ src/component/forms/accountSetupForm.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {
+  Fade,
   Grid,
   IconButton,
   Button,
@@ -37,114 +38,121 @@ const AccountSetupForm = ({ onNext }) => {
 
   return (
     <Container fixed maxWidth="md">
-      <Paper variant="outlined" elevation={2}>
-        <form className={root} onSubmit={handleSubmit}>
-          <Grid container direction="row" justify="center" alignItems="center">
-            <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
-              <Grid container spacing={3}>
-                <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
-                  {renderDropDown(items, "ID Type", "ID Type")}
-                </Grid>
-                <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
-                  {renderInput("id", "id", "ID Number", "text", "")}
-                </Grid>
-                <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                  {renderInput(
-                    "email",
-                    "email",
-                    "Email",
-                    "text",
-                    "You can use letters, numbers and periods"
-                  )}
-                </Grid>
-                <Grid item xs={12} sm={5} md={5} lg={5} xl={5}>
-                  {renderInput(
-                    "password",
-                    "password",
-                    "Password",
-                    values.showPassword ? "text" : "password",
-                    ""
-                  )}
-                </Grid>
-                <Grid item xs={12} sm={5} md={5} lg={5} xl={5}>
-                  {renderInput(
-                    "confirmpassword",
-                    "confirmpassword",
-                    "Confirm",
-                    values.showPassword ? "text" : "password",
-                    ""
-                  )}
-                </Grid>
-                <Grid item xs={2}>
-                  <IconButton
-                    style={{
-                      height: "35px",
-                    }}
-                    aria-label="toggle password visibility"
-                    onClick={handleClickShowPassword}
-                    onMouseDown={handleMouseDownPassword}
-                  >
-                    {values.showPassword ? <Visibility /> : <VisibilityOff />}
-                  </IconButton>
-                </Grid>
-                <Grid item xs={12}>
-                  <Grid
-                    container
-                    direction="row"
-                    justify="flex-end"
-                    alignItems="flex-end"
-                  >
-                    <Grid item xs>
-                      <Button
-                        href="#text-buttons"
-                        style={{
-                          textTransform: "none",
-                          padding: "5px 20px ",
-                          textAlign: "center",
-                          fontSize: "15px",
-                        }}
-                        color="primary"
-                      >
-                        Sign in instead
-                      </Button>
-                    </Grid>
-                    <Grid item xs>
-                      <Grid
-                        container
-                        direction="row"
-                        justify="flex-end"
-                        alignItems="flex-end"
-                      >
+      <Fade in={true}>
+        <Paper variant="outlined" elevation={2}>
+          <form className={root} onSubmit={handleSubmit}>
+            <Grid
+              container
+              direction="row"
+              justify="center"
+              alignItems="center"
+            >
+              <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+                <Grid container spacing={3}>
+                  <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
+                    {renderDropDown(items, "ID Type", "ID Type")}
+                  </Grid>
+                  <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
+                    {renderInput("id", "id", "ID Number", "text", "")}
+                  </Grid>
+                  <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                    {renderInput(
+                      "email",
+                      "email",
+                      "Email",
+                      "text",
+                      "You can use letters, numbers and periods"
+                    )}
+                  </Grid>
+                  <Grid item xs={12} sm={5} md={5} lg={5} xl={5}>
+                    {renderInput(
+                      "password",
+                      "password",
+                      "Password",
+                      values.showPassword ? "text" : "password",
+                      ""
+                    )}
+                  </Grid>
+                  <Grid item xs={12} sm={5} md={5} lg={5} xl={5}>
+                    {renderInput(
+                      "confirmpassword",
+                      "confirmpassword",
+                      "Confirm",
+                      values.showPassword ? "text" : "password",
+                      ""
+                    )}
+                  </Grid>
+                  <Grid item xs={2}>
+                    <IconButton
+                      style={{
+                        height: "35px",
+                      }}
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                    >
+                      {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Grid
+                      container
+                      direction="row"
+                      justify="flex-end"
+                      alignItems="flex-end"
+                    >
+                      <Grid item xs>
                         <Button
-                          variant="contained"
-                          color="primary"
+                          href="#text-buttons"
                           style={{
-                            padding: "5px 25px 8px ",
-                            fontSize: "15px",
+                            textTransform: "none",
+                            padding: "5px 20px ",
                             textAlign: "center",
+                            fontSize: "15px",
                           }}
-                          onClick={onNext}
+                          color="primary"
                         >
-                          Next
+                          Sign in instead
                         </Button>
+                      </Grid>
+                      <Grid item xs>
+                        <Grid
+                          container
+                          direction="row"
+                          justify="flex-end"
+                          alignItems="flex-end"
+                        >
+                          <Button
+                            variant="contained"
+                            color="primary"
+                            style={{
+                              padding: "5px 25px 8px ",
+                              fontSize: "15px",
+                              textAlign: "center",
+                            }}
+                            onClick={onNext}
+                          >
+                            Next
+                          </Button>
+                        </Grid>
                       </Grid>
                     </Grid>
                   </Grid>
                 </Grid>
               </Grid>
-            </Grid>
-            <Hidden smDown>
-              <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
-                <Grid container align="center" className={margin}>
-                  <Grid item xs>
-                    <img src={BueLogo} alt="" width={300} height={250} />
+              <Hidden smDown>
+                <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
+                  <Grid container align="center" className={margin}>
+                    <Grid item xs>
+                      <img src={BueLogo} alt="" width={300} height={250} />
+                    </Grid>
                   </Grid>
                 </Grid>
-              </Grid>
-            </Hidden>
-          </Grid>
-        </form>
-      </Paper>
+              </Hidden>
+            </Grid>
+          </form>
+        </Paper>
+      </Fade>
     </Container>
   );
 };
--- src/component/forms/personalInformationForm.jsx
+++ src/component/forms/personalInformationForm.jsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { Grid, Button, withWidth, Paper, Container } from "@material-ui/core";
+import {
+  Fade,
+  Grid,
+  Button,
+  withWidth,
+  Paper,
+  Container,
+} from "@material-ui/core";
 
 import PropTypes from "prop-types";
 import { Form, UseStyle } from "../form";
@@ -29,117 +36,126 @@ const PersonalInformationForm = ({ onNext, onBack, activeStep, steps }) => {
   return (
     // <Paper className={root}>
     <Container fixed maxWidth="md">
-      <Paper variant="outlined" elevation={2}>
-        <form className={root} onSubmit={handleSubmit}>
-          <Grid container direction="row" justify="center" alignItems="center">
-            <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-              <Grid container spacing={3}>
-                <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
-                  {renderInput(
-                    "firstname",
-                    "firstname",
-                    "First Name",
-                    "text",
-                    ""
-                  )}
-                </Grid>
-                <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
-                  {renderInput(
-                    "middlename",
-                    "middlename",
-                    "Middle Name",
-                    "text",
-                    ""
-                  )}
-                </Grid>
-                <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
-                  {renderInput(
-                    "secondname",
-                    "secondname",
-                    "Last Name",
-                    "text",
-                    ""
-                  )}
-                </Grid>
-                <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
-                  {renderDropDown(genderItems, "Gender", "gender")}
-                </Grid>
-                <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
-                  {renderDropDown(religionItems, "Religion", "religion")}
-                </Grid>
-                <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
-                  {renderDropDown(
-                    nationalityItems,
-                    "Natioanlity",
-                    "nationality"
-                  )}
-                </Grid>
-                <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
-                  {renderDropDown(
-                    nationalityItems,
-                    "Place of Birth",
-                    "placeofbirth"
-                  )}
-                </Grid>
-                <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
-                  {renderDropDown(
-                    maritalStatus,
-                    "Marital Status",
-                    "Marital Status"
-                  )}
-                </Grid>
-                <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                  <DatePicker />
-                </Grid>
-                <Grid item xs={12}>
-                  <Grid
-                    container
-                    direction="row"
-                    justify="flex-end"
-                    alignItems="flex-end"
-                  >
-                    <Grid item xs>
-                      <Grid
-                        container
-                        direction="row"
-                        justify="center"
-                        alignItems="center"
-                      >
-                        <Button
-                          variant="contained"
-                          color="primary"
-                          style={{
-                            padding: "5px 25px 8px ",
-                            fontSize: "15px",
-                            textAlign: "center",
-                            marginRight: "20px",
-                          }}
-                          onClick={onBack}
-                          disabled={activeStep === 0}
+      <Fade in={true}>
+        <Paper variant="outlined" elevation={2}>
+          <form className={root} onSubmit={handleSubmit}>
+            <Grid
+              container
+              direction="row"
+              justify="center"
+              alignItems="center"
+            >
+              <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                <Grid container spacing={3}>
+                  <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
+                    {renderInput(
+                      "firstname",
+                      "firstname",
+                      "First Name",
+                      "text",
+                      ""
+                    )}
+                  </Grid>
+                  <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
+                    {renderInput(
+                      "middlename",
+                      "middlename",
+                      "Middle Name",
+                      "text",
+                      ""
+                    )}
+                  </Grid>
+                  <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
+                    {renderInput(
+                      "secondname",
+                      "secondname",
+                      "Last Name",
+                      "text",
+                      ""
+                    )}
+                  </Grid>
+                  <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
+                    {renderDropDown(genderItems, "Gender", "gender")}
+                  </Grid>
+                  <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
+                    {renderDropDown(religionItems, "Religion", "religion")}
+                  </Grid>
+                  <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
+                    {renderDropDown(
+                      nationalityItems,
+                      "Natioanlity",
+                      "nationality"
+                    )}
+                  </Grid>
+                  <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
+                    {renderDropDown(
+                      nationalityItems,
+                      "Place of Birth",
+                      "placeofbirth"
+                    )}
+                  </Grid>
+                  <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
+                    {renderDropDown(
+                      maritalStatus,
+                      "Marital Status",
+                      "Marital Status"
+                    )}
+                  </Grid>
+                  <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                    <DatePicker />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Grid
+                      container
+                      direction="row"
+                      justify="flex-end"
+                      alignItems="flex-end"
+                    >
+                      <Grid item xs>
+                        <Grid
+                          container
+                          direction="row"
+                          justify="center"
+                          alignItems="center"
                         >
-                          Back
-                        </Button>
-                        <Button
-                          variant="contained"
-                          color="primary"
-                          style={{
-                            padding: "5px 25px 8px ",
-                            fontSize: "15px",
-                            textAlign: "center",
-                          }}
-                          onClick={onNext}
-                        >
-                          {activeStep === steps.length - 1 ? "Finish" : "Next"}
-                        </Button>
+                          <Button
+                            variant="contained"
+                            color="primary"
+                            style={{
+                              padding: "5px 25px 8px ",
+                              fontSize: "15px",
+                              textAlign: "center",
+                              marginRight: "20px",
+                            }}
+                            onClick={onBack}
+                            disabled={activeStep === 0}
+                          >
+                            Back
+                          </Button>
+                          <Button
+                            variant="contained"
+                            color="primary"
+                            style={{
+                              padding: "5px 25px 8px ",
+                              fontSize: "15px",
+                              textAlign: "center",
+                            }}
+                            onClick={onNext}
+                          >
+                            {activeStep === steps.length - 1
+                              ? "Finish"
+                              : "Next"}
+                          </Button>
+                        </Grid>
                       </Grid>
                     </Grid>
                   </Grid>
                 </Grid>
               </Grid>
             </Grid>
-          </Grid>
-        </form>
-      </Paper>
+          </form>
+        </Paper>
+      </Fade>
     </Container>
   );
 };
